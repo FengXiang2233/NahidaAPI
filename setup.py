e3b2915a4bab017aaca17ba54c992b15761e2820
fengxiang2233 <fjx13125@163.com>
2024-08-11 12:24:09 +0800

    init

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="NahidaAPI",
+    version="0",
+    packages=find_packages(),
+    url="https://github.com/FengXiang2233/NahidaAPI",
+    license="GPL-3",
+    author="fengxiang",
+    description="一个基于QQ官方openAPI支持插件加载的QQBot框架",
+    requires=">=3.8"
+)
